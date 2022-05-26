7f075ba59a476330d0fd2d98deba554acb8f02e6
vvprivalov <vv.privalov@yandex.ru>
2022-05-26 07:13:47 +0300

    1

---
+++ src/main/java/ru/geekbrains/webappproduct/WebAppProductApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.webappproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppProductApplication.class, args);
+	}
+
+}
